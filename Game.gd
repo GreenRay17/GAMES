80da279970fb847d6c43304ea2a05242a3dce728
Kolido <koli@o>
2024-06-30 13:42:16 +0200

    toggle des items à acheter

--- Game.gd
+++ Game.gd
@@ -1,7 +1,7 @@
 extends Control
 
-var score = 0
-var diamant = 0
+var gold = 0
+var diamond = 0
 var add = 1
 var addpersec = 1
 
@@ -33,9 +33,9 @@ func _getSave():
 		return
 	var file = FileAccess.open("res://save.txt", FileAccess.READ)
 	
-	var score_var = file.get_var()
-	if score_var != null:
-		score = score_var
+	var gold_var = file.get_var()
+	if gold_var != null:
+		gold = gold_var
 		
 	var add_var = file.get_var()
 	if add_var != null:
@@ -60,120 +60,175 @@ func _getSave():
 	file.close()
 	
 func _process(_delta):
-	$GoldValue.text = str(score) #Change the text to the current score every frame.
-	$DiamondValue.text = str(diamant)
-				
+	$GoldValue.text = str(gold) #Change the text to the current gold every frame.	
+	_toggleCpcItem()
+	$DiamondValue.text = str(diamond)
+	_toggleCpsItem()
+	
+func _toggleCpcItem():
+	if gold >= config.CPCRequirement:
+		$MenuBoutique/CPC1.disabled = false
+	else:
+		$MenuBoutique/CPC1.disabled = true
+		
+	if gold >= config.CPCRequirement2:
+		$MenuBoutique/CPC2.disabled = false
+	else:
+		$MenuBoutique/CPC2.disabled = true
+	
+	if gold >= config.CPCRequirement3:
+		$MenuBoutique/CPC3.disabled = false
+	else:
+		$MenuBoutique/CPC3.disabled = true
+		
+	if gold >= config.CPCRequirement4:
+		$MenuBoutique/CPC4.disabled = false
+	else:
+		$MenuBoutique/CPC4.disabled = true
+		
+	if gold >= config.CPCRequirement5:
+		$MenuBoutique/CPC5.disabled = false
+	else:
+		$MenuBoutique/CPC5.disabled = true
+		
+func _toggleCpsItem():
+	if diamond >= config.CPSRequirement:
+		$MenuBoutique/CPS1.disabled = false
+	else:
+		$MenuBoutique/CPS1.disabled = true
+		
+	if diamond >= config.CPSRequirement2:
+		$MenuBoutique/CPS2.disabled = false
+	else:
+		$MenuBoutique/CPS2.disabled = true
+	
+	if diamond >= config.CPSRequirement3:
+		$MenuBoutique/CPS3.disabled = false
+	else:
+		$MenuBoutique/CPS3.disabled = true
+		
+	if diamond >= config.CPSRequirement4:
+		$MenuBoutique/CPS4.disabled = false
+	else:
+		$MenuBoutique/CPS4.disabled = true
+		
+	if diamond >= config.CPSRequirement5:
+		$MenuBoutique/CPS5.disabled = false
+	else:
+		$MenuBoutique/CPS5.disabled = true
+						
 func _input(event):
 	if (event is InputEventKey and not event.is_echo()):
 		if event.pressed and event.keycode == KEY_ESCAPE:
 			$MenuBoutique.visible = !$MenuBoutique.visible
 			
 func _on_Timer_timeout():
-	score += addpersec #After the Timer resets, add the add per second to the score.
+	gold += addpersec #After the Timer resets, add the add per second to the gold.
 	updateLevel(addpersec)	
 	$BossLife.value = currentLife
 		
 func _on_CPC1_pressed():
-	if score >= config.CPCRequirement:
-		score -= config.CPCRequirement
+	if gold >= config.CPCRequirement:
+		gold -= config.CPCRequirement
 		config.CPCRequirement = round(config.CPCRequirement * 1.4)
 		add = add + 1 #Add CPC
-		var msg = str("+1 CPC [", config.CPCRequirement, "]")
+		var msg = str("+1 CPC [", config.CPCRequirement, " GOLD]")
 		$MenuBoutique/CPC1.text = msg #Combine multiple strings to show the required clicks.
 		$CPC.text = str("CPC:", add)
 		displayBought($MenuBoutique/BoughtCPC, msg)
 
 func _on_CPC2_pressed():
-	if score >= config.CPCRequirement2:
-		score -= config.CPCRequirement2
+	if gold >= config.CPCRequirement2:
+		gold -= config.CPCRequirement2
 		config.CPCRequirement2 = round(config.CPCRequirement2 * 1.3)
 		add = add + 5 #Add CPC
-		var msg = str("+5 CPC [", config.CPCRequirement2, "]")
+		var msg = str("+5 CPC [", config.CPCRequirement2, " GOLD]")
 		$MenuBoutique/CPC2.text = msg#Combine multiple strings to show the required clicks.
 		$CPC.text = str("CPC:", add)
 		displayBought($MenuBoutique/BoughtCPC, msg)
 	
 func _on_CPC3_pressed():
-	if score >= config.CPCRequirement3:
-		score -= config.CPCRequirement3
+	if gold >= config.CPCRequirement3:
+		gold -= config.CPCRequirement3
 		config.CPCRequirement3 = round(config.CPCRequirement3 * 1.2)
 		add = add + 20 #Add CPC
-		var msg = str("+20 CPC [", config.CPCRequirement3, "]")
+		var msg = str("+20 CPC [", config.CPCRequirement3, " GOLD]")
 		$MenuBoutique/CPC3.text = msg #Combine multiple strings to show the required clicks.
 		$CPC.text = str("CPC:", add)
 		displayBought($MenuBoutique/BoughtCPC, msg)
 
 func _on_CPC4_pressed():
-	if score >= config.CPCRequirement4:
-		score -= config.CPCRequirement4
+	if gold >= config.CPCRequirement4:
+		gold -= config.CPCRequirement4
 		config.CPCRequirement4 = round(config.CPCRequirement4 * 1.1)
 		add = add + 125 #Add CPC
-		var msg = str("+125 CPC [", config.CPCRequirement4, "]")
+		var msg = str("+125 CPC [", config.CPCRequirement4, " GOLD]")
 		$MenuBoutique/CPC4.text = msg #Combine multiple strings to show the required clicks.
 		$CPC.text = str("CPC:", add)		
 		displayBought($MenuBoutique/BoughtCPC, msg)
 	
 func _on_CPC5_pressed():
-	if score >= config.CPCRequirement5:
-		score -= config.CPCRequirement5
+	if gold >= config.CPCRequirement5:
+		gold -= config.CPCRequirement5
 		config.CPCRequirement5 = config.CPCRequirement5
 		add = add + 500 #Add CPC
-		var msg = str("+500 CPC [", config.CPCRequirement5, "]")
+		var msg = str("+500 CPC [", config.CPCRequirement5, " GOLD]")
 		$MenuBoutique/CPC5.text = msg #Combine multiple strings to show the required clicks.
 		$CPC.text = str("CPC:", add)	
 		displayBought($MenuBoutique/BoughtCPC, msg)
 		
 func _on_CPS1_pressed():
-	if diamant >= config.CPSRequirement:
-		diamant -= config.CPSRequirement
+	if diamond >= config.CPSRequirement:
+		diamond -= config.CPSRequirement
 		config.CPSRequirement = round(config.CPSRequirement * 1.4)
 		addpersec = addpersec + 1 #Add CPS.
-		var msg = str("+1 CPS [", config.CPSRequirement, "]") 
+		var msg = str("+1 CPS [", config.CPSRequirement, " DIAMOND]") 
 		$MenuBoutique/CPS1.text = msg#Combine multiple strings to show the required clicks.
 		$CPS.text = str("CPS:", addpersec)
 		displayBought($MenuBoutique/BoughtCPS, msg)
 
 func _on_CPS2_pressed():
-	if diamant >= config.CPSRequirement2:
-		diamant -= config.CPSRequirement2
+	if diamond >= config.CPSRequirement2:
+		diamond -= config.CPSRequirement2
 		config.CPSRequirement2 = round(config.CPSRequirement2 * 1.3)
 		addpersec = addpersec + 5 #Add CPS.
-		var msg = str("+5 CPS [", config.CPSRequirement2, "]")
+		var msg = str("+5 CPS [", config.CPSRequirement2, " DIAMOND]")
 		$MenuBoutique/CPS2.text = msg # Combine multiple strings to show the required clicks.
 		$CPS.text = str("CPS:", addpersec)
 		displayBought($MenuBoutique/BoughtCPS, msg)
 
 func _on_CPS3_pressed():
-	if score >= config.CPSRequirement3:
-		score -= config.CPSRequirement3
+	if gold >= config.CPSRequirement3:
+		gold -= config.CPSRequirement3
 		config.CPSRequirement3 = round(config.CPSRequirement3 * 1.2)
 		addpersec = addpersec + 20 #Add CPS.
-		var msg = str("+20 CPS [", config.CPSRequirement3, "]") 
+		var msg = str("+20 CPS [", config.CPSRequirement3, " DIAMOND]") 
 		$MenuBoutique/CPS3.text = msg#Combine multiple strings to show the required clicks.
 		$CPS.text = str("CPS:", addpersec)
 		displayBought($MenuBoutique/BoughtCPS, msg)
 
 func _on_CPS4_pressed():
-	if score >= config.CPSRequirement4:
-		score -= config.CPSRequirement4
+	if gold >= config.CPSRequirement4:
+		gold -= config.CPSRequirement4
 		config.CPSRequirement4 = round(config.CPSRequirement4 * 1.1)
 		addpersec = addpersec + 125 #Add CPS.
-		var msg = str("+125 CPS [", config.CPSRequirement4, "]")
+		var msg = str("+125 CPS [", config.CPSRequirement4, " DIAMOND]")
 		$MenuBoutique/CPS4.text = msg #Combine multiple strings to show the required clicks.
 		$CPS.text = str("CPS:", addpersec)
 		displayBought($MenuBoutique/BoughtCPS, msg)
 
 func _on_CPS5_pressed():
-	if score >= config.CPSRequirement5:
-		score -= config.CPSRequirement5
+	if gold >= config.CPSRequirement5:
+		gold -= config.CPSRequirement5
 		config.CPSRequirement5 = config.CPSRequirement5
 		addpersec = addpersec + 500 #Add CPS.
-		var msg = str("+500 CPS [", config.CPSRequirement5, "]")
+		var msg = str("+500 CPS [", config.CPSRequirement5, " DIAMOND]")
 		$MenuBoutique/CPS5.text = msg #Combine multiple strings to show the required clicks.
 		$CPS.text = str("CPS:", addpersec)	
 		displayBought($MenuBoutique/BoughtCPS, msg)
 	
 func displayBought(bought, text):
+	bought.text = text
 	var original_position = bought.position
 	for i in range(30):
 		bought.position.y -= 3
@@ -184,7 +239,7 @@ func displayBought(bought, text):
 func _on_button_monster_button_down():
 	Input.set_custom_mouse_cursor(config.epee_onclick)
 	
-	score += add # Replace with function body.   
+	gold += add # Replace with function body.   
 	
 	var particles = particles_scene.instantiate()
 	add_child(particles)        
@@ -194,13 +249,16 @@ func _on_button_monster_button_down():
 	$Monster/HitSound.play()		
 	$BossLife.value = currentLife
 	
+	_toggleCpcItem()
+	_toggleCpsItem()
+	
 func _on_button_monster_button_up():
 	Input.set_custom_mouse_cursor(config.epee_cursor)
 	
 func updateLevel(add):	
 	currentLife -= add	
 	if currentLife <= 0:
-		diamant += add
+		diamond += add
 		level += 1
 		maxLife = round(maxLife*level)
 		currentLife = maxLife		
@@ -229,7 +287,7 @@ func _saveSave():
 		return
 		
 	var file = FileAccess.open("res://save.txt", FileAccess.READ_WRITE)
-	file.store_var(score)
+	file.store_var(gold)
 	file.store_var(add)
 	file.store_var(addpersec)
 	file.store_var(level)
